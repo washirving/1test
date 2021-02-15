dbefcda179ad71db4e87a5a4d0037b5c81b80833
washirving <anonim359@gmail.com>
2021-02-15 13:12:11 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -2,5 +2,17 @@
 
 echo 111;
 
+$params = array(
+	'owner_id' => '-55353884',
+	'album_id' => 'wall',
+	'count' => '1',
+	'offset' => rand(1,12000),
+	'v' => '5.85'
+);
+$url = "https://api.vk.com/method/photos.get?" . http_build_query($params);
+$data = file_get_contents($url);
+$response = json_decode($data);
+$img = $response->response->items[0]->photo_604;
 
-echo 333;
+
+echo $response;
