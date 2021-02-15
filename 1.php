cf75da04232da0b097389610b5e3d9714cab4f2e
washirving <anonim359@gmail.com>
2021-02-15 11:51:06 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,3 +1,7 @@
 <?php
 
 echo 111;
+
+$q = file_get_contents(https://api.telegram.org/bot1030065277:AAE7_0RpV8CuiblFUx0FzqG7GS_OpS-uomo/sendMessage?chat_id=@aakjsdfhkajsfhkjsadfasyryryyryry&text="test");
+                                      
+echo $q;
